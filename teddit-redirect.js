7e2fb8af838b5d5082ae199b114a03d75f8371ea
Jeff <67169241+JE-FF@users.noreply.github.com>
2021-01-15 13:16:29 -0600

    Update teddit-redirect.js
    remove redundant code

--- teddit-redirect.js
+++ teddit-redirect.js
@@ -9,7 +9,7 @@ function onSettingsRead(item) {
   }
 
   const original_url = window.location.href
-  const new_url = original_url.replace('www.youtube.com', instance).replace('youtube.com', instance)
+  const new_url = original_url.replace('www.youtube.com', instance)
   window.location.replace(new_url)
 }
 
